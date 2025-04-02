22f16c13eb9e58b857952d8bc42d2798ae9d7e55
Banothu Sonusree <sonusree.b2006@gmail.com>
2025-04-02 17:07:51 +0530

    Add setup page: init commit

---
+++ setup-page/score.js
@@ -0,0 +1,17 @@
+/* function 1 */
+
+/* define a function that takes the input from the player name form and creates a new table entry */
+/* if team is full (11 players) switch to next team*/
+/* if both teams are full, end. */
+
+
+/* function 2 */
+
+/* define a function that lets people chjoose number of overs */
+/* by default it is 2*/
+
+
+/* function 3 */
+
+/* define a function that gives toss */
+/* it shoul dbe such that when called, it chooses between 4 phrases : team 1/2 won toss, choose bat/field*/
