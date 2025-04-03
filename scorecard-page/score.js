aa4d0080a0e6fe34faf5200cd0617785eb54be71
Banothu Sonusree <sonusree.b2006@gmail.com>
2025-04-04 02:30:40 +0530

    Add scorecard page: init commit

---
+++ scorecard-page/score.js
@@ -0,0 +1,17 @@
+/* fucntion 1  */
+
+/* a function thatoin pressnigthebuttontakes back to live.html*/
+
+function go_to_live() {
+    window.location.href = "live.html";
+}
+
+document.getElementById("btn").addEventListener("click", goToLivePage);
+
+function batting_table_change() {
+
+}
+
+function bowling_table_change() {
+    
+}
